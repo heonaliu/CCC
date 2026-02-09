48a79aa82c8cf73d0a91c8538d943ef50d683199
Heona Liu <heonaliu@gmail.com>
2026-02-08 20:33:00 -0500

    j3-24 change path, comment extra print

--- 2024/j3-bronze-count.py
+++ 2024/j3-bronze-count.py
@@ -1,11 +1,11 @@
-path = "/Users/heonaliu/Desktop/Projects/CCC/2024/j3-bronze-count-input.txt"
+path = "2024/j3-bronze-count-input.txt"
 
 with open(path, 'r') as file:
     lines_list = file.readlines()
 
 lines_list = [int(i.strip()) for i in lines_list]
 num = lines_list[0]
-print(lines_list)
+
 
 lines_list.remove(num)
 lines_list.sort()
@@ -13,7 +13,7 @@ max = lines_list[-1]
 silver = 0
 silver_index = 0
 bronze = 0
-print(lines_list)
+
 for i in range(num-1, -1, -1):
     if (lines_list[i] != max):
         #first silver occurrence
@@ -21,17 +21,17 @@ for i in range(num-1, -1, -1):
         silver_index = i
         break
 
-print(lines_list)
+#print(lines_list)
 #returns a new list with last being silver
 del lines_list[silver_index+1:]
-print(lines_list)
+#print(lines_list)
 
 for i in range(len(lines_list)-1, -1, -1):
     if (lines_list[i] != silver):
         #first silver occurrence
         bronze = lines_list[i]
         break
-print(bronze)
+#print(bronze)
 bronze_count = 0
 for i in range(len(lines_list)):
     if (lines_list[i] == bronze):
